d9a6ad342799eecd26a17e2dc45f3ee406b26f50
Flavio <flavio.fernandes1200@gmail.com>
2023-03-05 17:35:03 -0300

    Created authorization server and resource server

--- spring-security-client/src/main/java/com/flavio/springsecurityclient/service/UserServiceImpl.java
+++ spring-security-client/src/main/java/com/flavio/springsecurityclient/service/UserServiceImpl.java
@@ -26,6 +26,7 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private PasswordResetTokenRepository passwordResetTokenRepository;
+
     @Autowired
     private PasswordEncoder passwordEncoder;
 
@@ -44,37 +45,39 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void saveVerificationTokenForUser(String token, User user) {
-        VerificationToken verificationToken = new VerificationToken(user, token);
+        VerificationToken verificationToken
+                = new VerificationToken(user, token);
 
         verificationTokenRepository.save(verificationToken);
     }
 
     @Override
     public String validateVerificationToken(String token) {
-        VerificationToken verificationToken = verificationTokenRepository.findByToken(token);
+        VerificationToken verificationToken
+                = verificationTokenRepository.findByToken(token);
 
-        if(verificationToken == null) {
+        if (verificationToken == null) {
             return "invalid";
         }
 
         User user = verificationToken.getUser();
-        Calendar calendar = Calendar.getInstance();
+        Calendar cal = Calendar.getInstance();
 
-        if((verificationToken.getExpirationTime().getTime() - calendar.getTime().getTime()) <= 0) {
+        if ((verificationToken.getExpirationTime().getTime()
+                - cal.getTime().getTime()) <= 0) {
             verificationTokenRepository.delete(verificationToken);
-
             return "expired";
         }
 
         user.setEnabled(true);
         userRepository.save(user);
-
         return "valid";
     }
 
     @Override
     public VerificationToken generateNewVerificationToken(String oldToken) {
-        VerificationToken verificationToken = verificationTokenRepository.findByToken(oldToken);
+        VerificationToken verificationToken
+                = verificationTokenRepository.findByToken(oldToken);
         verificationToken.setToken(UUID.randomUUID().toString());
         verificationTokenRepository.save(verificationToken);
         return verificationToken;
@@ -87,24 +90,26 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void createPasswordResetTokenForUser(User user, String token) {
-        PasswordResetToken passwordResetToken = new PasswordResetToken(user, token);
+        PasswordResetToken passwordResetToken
+                = new PasswordResetToken(user,token);
         passwordResetTokenRepository.save(passwordResetToken);
     }
 
     @Override
     public String validatePasswordResetToken(String token) {
-        PasswordResetToken passwordResetToken = passwordResetTokenRepository.findByToken(token);
+        PasswordResetToken passwordResetToken
+                = passwordResetTokenRepository.findByToken(token);
 
-        if(passwordResetToken == null) {
+        if (passwordResetToken == null) {
             return "invalid";
         }
 
         User user = passwordResetToken.getUser();
-        Calendar calendar = Calendar.getInstance();
+        Calendar cal = Calendar.getInstance();
 
-        if((passwordResetToken.getExpirationTime().getTime() - calendar.getTime().getTime()) <= 0) {
+        if ((passwordResetToken.getExpirationTime().getTime()
+                - cal.getTime().getTime()) <= 0) {
             passwordResetTokenRepository.delete(passwordResetToken);
-
             return "expired";
         }
 
